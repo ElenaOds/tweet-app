2001d4a5764758eb75eff58ecedb74a25aea3274
Elena Pedko <elpg.ods@gmail.com>
2023-05-21 21:08:15 +0300

    button is changed

--- src/components/PaginationButton/PaginationButton.jsx
+++ src/components/PaginationButton/PaginationButton.jsx
@@ -1,12 +1,12 @@
 
 import { Button } from './PaginationButton.styled';
 
-const PaginationButton = ({onLoadMore, isDisabled}) => {
+const PaginationButton = ({onLoadMore}) => {
     
    
   return (
     <div>
-      <Button onClick={onLoadMore} disabled={isDisabled}>Load More</Button> 
+      <Button onClick={onLoadMore}>Load More</Button> 
     </div> 
   );
 }
